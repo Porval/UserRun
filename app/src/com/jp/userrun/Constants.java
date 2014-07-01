4dac1930702ca4b5f9e0d1842daa8243419163a0
jiangpeng <jiangpeng@douban.com>
2014-07-01 12:33:01 +0800

    create toy model

---
+++ app/src/com/jp/userrun/Constants.java
@@ -0,0 +1,6 @@
+package com.jp.userrun;
+
+public class Constants {
+    public static int TRUE = 0;
+    public static int FALSE = 1;
+}
--- app/src/com/jp/userrun/data/Toy.java
+++ app/src/com/jp/userrun/data/Toy.java
@@ -2,4 +2,15 @@ package com.jp.userrun.data;
 
 public class Toy {
 
+    public int id;
+
+    public String name;
+
+    public int isInstalled;
+
+    public int isPlugin;
+
+    public long lastOperationTime;
+
+    public String icon;
 }
